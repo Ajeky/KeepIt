65929ea7c2a5da90a7b0ee50a933cf3784a40c45
Ajeky <alvaromarquez98@gmail.com>
2019-12-12 14:30:48 +0100

    Crear nota empezado

--- src/app/crear-nota-dialog/crear-nota-dialog.component.ts
+++ src/app/crear-nota-dialog/crear-nota-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { NotasService } from '../services/notas.service';
+import { Nota } from '../models/nota.interface';
 
 @Component({
   selector: 'app-crear-nota-dialog',
@@ -6,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-nota-dialog.component.scss'],
 })
 export class CrearNotaDialogComponent implements OnInit {
+  nota: Nota;
 
-  constructor() { }
+  constructor(
+    public modalController: ModalController,
+    public notasServicio: NotasService
+  ) { }
 
   ngOnInit() {}
 
+  cerrar() {
+    this.modalController.dismiss();
+  }
 }
---
+++ src/app/models/notas.dto.ts
@@ -0,0 +1,9 @@
+export class NotaDto {
+    constructor (
+        public titulo: string,
+        public descripcion: string,
+        public categoria: string
+    ) {  }
+
+    
+}
